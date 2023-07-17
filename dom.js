64d5264c6a6ad770c380eafae4fa3eb4a02f0111
Vivek Kumar <ui.vivekk@gmail.com>
2023-07-17 21:19:45 +0530

    Added bg and font weight

--- dom.js
+++ dom.js
@@ -5,4 +5,12 @@ header.style.border = "2px solid black";
 additem.style.fontWeight = "bolder"
 additem.style.color = "green"
 
+const listItem = document.getElementsByClassName("list-group-item");
+
+listItem[2].style.backgroundColor = "green";
+
+for(let list of listItem) {
+  list.style.fontWeight = "bolder"
+}
+
 
