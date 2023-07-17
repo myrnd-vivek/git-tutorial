ba7b675a8101f590ee9d0b8270dc568e4322e3f6
Vivek Kumar <ui.vivekk@gmail.com>
2023-07-17 21:10:25 +0530

    Added Dom Manipulation Example

---
+++ dom.js
@@ -0,0 +1,8 @@
+const header = document.getElementById('main-header');
+const additem = document.getElementById('add-item');
+
+header.style.border = "2px solid black";
+additem.style.fontWeight = "bolder"
+additem.style.color = "green"
+
+
