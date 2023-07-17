68e2e1b756110564cde558e8dfa6d40f2bcec895
Vivek Kumar <ui.vivekk@gmail.com>
2023-07-17 22:37:39 +0530

    Added question 7

--- dom.js
+++ dom.js
@@ -1,22 +1,76 @@
-const ul = document.querySelector("#items");
+/*===== Traversing the DOM =====*/
 
-const li = document.createElement("li");
-li.textContent = 'Item 5';
-ul.appendChild(li)
+const itemList = document.querySelector("#items");
+
+// Parent Node
+
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = "#f5f7f9"
+
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = "#f5f7f9"
+
+// Child Node 
+
+// console.log(itemList.childNodes)
+// console.log(itemList.children)
+// console.log(itemList.children[1])
+// itemList.children[2].style.backgroundColor = 'yellow'
+
+// First Child 
+
+// console.log(itemList.firstChild)
+// console.log(itemList.firstElementChild)
+// itemList.firstElementChild.textContent = "hello world"
+
+// Last Child
+
+// console.log(itemList.lastChild)
+// console.log(itemList.lastElementChild)
+// itemList.lastElementChild.style.color = 'green'
+ 
+// Next Sibling
+
+// console.log(itemList.nextSibling)
+// console.log(itemList.nextElementSibling)
+
+// Previous Sibling
+
+// console.log(itemList.previousSibling)
+// console.log(itemList.previousElementSibling)
 
 
-const items = document.getElementsByClassName("list-group-item");
+// Create Element
 
-for(let item of items) {
-  item.style.backgroundColor = "#f5f9f7";
-}
+const newDiv = document.createElement("div");
 
-const items2 = document.getElementsByTagName("li");
-for(let item of items2) {
-  item.style.fontWeight = "bolder";
-}
+// Adding Class
 
-const secondEl = document.querySelector(".list-group-item:nth-child(2)");
-const thirdEl = document.querySelector(".list-group-item:nth-child(3)");
-secondEl.style.backgroundColor = 'green';
-thirdEl.style.display = 'none';
+newDiv.className = "hello";
+
+// Adding id
+
+newDiv.id = "hello1";
+
+// Adding attribute
+
+newDiv.setAttribute('titel','hello div');
+
+// Create text node
+
+const newDivText = document.createTextNode("hello world");
+
+// Add text to div
+newDiv.appendChild(newDivText)
+
+
+const container = document.querySelector("header .container");
+const h1 = document.querySelector("header h1")
+
+container.insertBefore(newDiv,h1)
+console.log(newDiv)
+
+const li = document.createElement("li")
+const items = document.querySelector("#items");
+const item = document.querySelector(".list-group-item:nth-child(1)")
+items.insertBefore(newDiv,item)
