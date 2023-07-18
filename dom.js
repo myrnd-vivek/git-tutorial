7672769bd4a16d8872a5a8d8c759b7c3da9b016a
Vivek Kumar <ui.vivekk@gmail.com>
2023-07-18 15:25:08 +0530

    Added Search Functionality

--- dom.js
+++ dom.js
@@ -1,13 +1,16 @@
 const form = document.querySelector("#addForm");
 const itemList = document.querySelector("#items");
+const filter = document.getElementById('filter');
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	const value = document.getElementById("item").value;
+	const name = document.getElementById("item");
+	const description = document.getElementById("desc");
 	
   const li = document.createElement("li");
 	li.className = "list-group-item";
-	li.appendChild(document.createTextNode(value));
+	li.appendChild(document.createTextNode(name.value));
+	li.appendChild(document.createTextNode(" " + description.value));
 
 	const button = document.createElement("button");
 	button.className = "btn btn-danger btn-sm float-right delete";
@@ -15,6 +18,8 @@ form.addEventListener("submit", (e) => {
 	li.appendChild(button);
   
 	itemList.appendChild(li);
+  name.value = '';
+  description.value = ''
 });
 
 
@@ -25,4 +30,18 @@ itemList.addEventListener("click",(e) => {
       itemList.removeChild(li);
     }
   }
+})
+
+filter.addEventListener("keyup",(e) => {
+  const text = e.target.value.toLowerCase();
+  const items = itemList.getElementsByTagName("li");
+  Array.from(items).forEach((item) => {
+    const itemName = item.firstChild.textContent;
+    const itemDesp = item.childNodes[1].textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1 || itemDesp.toLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  })
 })
